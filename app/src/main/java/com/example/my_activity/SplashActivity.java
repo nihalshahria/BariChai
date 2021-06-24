44302e843106d4b2ecad62b108e5e01f557a8a3a
nihalshahria <nihalshahria@gmail.com>
2021-06-25 03:17:42 +0600

    Login screen UI added

--- app/src/main/java/com/example/my_activity/SplashActivity.java
+++ app/src/main/java/com/example/my_activity/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent homeIntent = new Intent(SplashActivity.this, LogInScreenActivity.class);
                 startActivity(homeIntent);
                 finish();
             }
