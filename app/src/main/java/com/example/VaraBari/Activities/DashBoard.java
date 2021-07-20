45c5c4b34ecbeb7687984a6d89b0ac8959728025
Md. Shahria Islam Nihal <53012643+nihalshahria@users.noreply.github.com>
2021-07-20 22:51:10 +0600

    Update DashBoard.java

--- app/src/main/java/com/example/VaraBari/Activities/DashBoard.java
+++ app/src/main/java/com/example/VaraBari/Activities/DashBoard.java
@@ -9,9 +9,13 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -34,11 +38,14 @@ import com.squareup.picasso.Picasso;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class DashBoard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private View headerView;
     private RecyclerView recyclerView;
+    public EditText searchView;
+    CharSequence search = "";
     public TextView navUserFullName;
     private ImageView navUserPic;
     private DrawerLayout drawerLayout;
@@ -48,7 +55,7 @@ public class DashBoard extends AppCompatActivity implements NavigationView.OnNav
     private String _profileImageLink, _fullName, _phoneNo, _address, _email;
 
     DashboardAdapter dashboardAdapter;
-    ArrayList<House>list;
+    ArrayList<House> list;
 
 
     private FirebaseAuth firebaseAuth;
@@ -76,20 +83,17 @@ public class DashBoard extends AppCompatActivity implements NavigationView.OnNav
         houseRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-//                synchronized (this){
-                    list.clear();
-                    for (DataSnapshot dataSnapshot : snapshot.getChildren()){
-                        for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
-                            House house = dataSnapshot1.getValue(House.class);
-                            if(house.isAvailable){
-                                list.add(house);
-                            }
+                list.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                        House house = dataSnapshot1.getValue(House.class);
+                        if (house.isAvailable) {
+                            list.add(house);
                         }
                     }
-                    dashboardAdapter.notifyDataSetChanged();
-
-//                    }
-
+                }
+//                Collections.sort(list, House.compareByHouseAreaDsc);
+                dashboardAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -99,11 +103,34 @@ public class DashBoard extends AppCompatActivity implements NavigationView.OnNav
             }
         });
 
+        // Searchbar
+        searchView = (EditText)findViewById(R.id.dashboard_searchview);
+        searchView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                dashboardAdapter.getFilter().filter(charSequence);
+                search = charSequence;
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        ////////////////////////////////////
+
+
+        // navigation drawer
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout_dashboard);
         navigationView = (NavigationView) findViewById(R.id.nav_view_dashboard);
         headerView = navigationView.getHeaderView(0);
-        navUserPic = (ImageView)headerView.findViewById(R.id.nav_user_pic);
-        navUserFullName = (TextView)headerView.findViewById(R.id.nav_user_full_name);
+        navUserPic = (ImageView) headerView.findViewById(R.id.nav_user_pic);
+        navUserFullName = (TextView) headerView.findViewById(R.id.nav_user_full_name);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -113,17 +140,19 @@ public class DashBoard extends AppCompatActivity implements NavigationView.OnNav
         toggle.syncState();
 
         navigationView.setNavigationItemSelectedListener(this);
+        ///////////////////////////////////////////////////////////////////
 
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 _fullName = dataSnapshot.child(uuid).child("fullName").getValue(String.class);
                 _profileImageLink = dataSnapshot.child(uuid).child("profileImageLink").getValue(String.class);
-                if(!_profileImageLink.isEmpty()) {
+                if (!_profileImageLink.isEmpty()) {
                     Picasso.get().load(_profileImageLink).into(navUserPic);
                 }
-                if(!_fullName.isEmpty())navUserFullName.setText(_fullName);
+                if (!_fullName.isEmpty()) navUserFullName.setText(_fullName);
             }
+
             @Override
             public void onCancelled(@NonNull @NotNull DatabaseError error) {
                 Toast.makeText(DashBoard.this, "Error!", Toast.LENGTH_SHORT).show();
@@ -141,6 +170,7 @@ public class DashBoard extends AppCompatActivity implements NavigationView.OnNav
         }
     }
 
+    // navigation drawer
     @Override
     public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
         switch (item.getItemId()) {
